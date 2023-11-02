fb05915b51756fbe4c097516cba9a6fdcec8fe8b
kimoto22 <n22m305@matsu.shimame-u.ac.jp>
2023-11-02 08:27:55 +0000

    Full calenderの導入

--- shift-app/shift-app/src/components/login.tsx
+++ shift-app/shift-app/src/components/login.tsx
@@ -2,11 +2,18 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import { Link } from 'react-router-dom'
 
-const buttonContainerStyle = {
-	display: 'flex',
-	justifyContent: 'center',
-	alignItems: 'center',
-	height: '100vh'
+type CustomDivElementProps = {
+	style?: React.CSSProperties
+}
+
+const buttonContainerStyle: CustomDivElementProps = {
+	style: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: '100vh'
+	}
 }
 
 const largeButtonStyle = {
@@ -17,19 +24,17 @@ const largeButtonStyle = {
 
 export const Login: React.FC = () => {
 	return (
-		<div style={buttonContainerStyle}>
-			<div>
-				<Link to="/sampleB">
-					<Button variant="info" style={largeButtonStyle}>
-						Go to Sample B
-					</Button>
-				</Link>
-				<Link to="/sampleC">
-					<Button variant="warning" style={largeButtonStyle}>
-						Go to Sample C
-					</Button>
-				</Link>
-			</div>
+		<div {...buttonContainerStyle}>
+			<Link to="/sampleB">
+				<Button variant="info" style={largeButtonStyle}>
+					Go to Sample B
+				</Button>
+			</Link>
+			<Link to="/sampleC">
+				<Button variant="warning" style={largeButtonStyle}>
+					Go to Sample C
+				</Button>
+			</Link>
 		</div>
 	)
 }
